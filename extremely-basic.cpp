783c07df7493bfff09e2a7ec13ba970c3243e6ec
Erick Ribeiro <erickrocha965@gmail.com>
2022-02-16 09:35:03 -0300

    exercicios aula 1

---
+++ extremely-basic.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> //biblioteca para funções de entrada e saida de dados
+
+int main () { //inicio do algoritmo
+	int a, b, x;
+	printf("Digite o primeiro numero: ");
+	scanf("%d", &a);
+	printf("Digite o segundo numero: ");
+	scanf("%d", &b);
+	x = a + b;
+	printf("X = %d", x);
+	
+}//final do algoritmo
