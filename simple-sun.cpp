665ced485d05cf4318d02bb81d484cc73830e96f
Erick Ribeiro <erickrocha965@gmail.com>
2022-02-16 09:42:10 -0300

    exercicios aula 2

--- simple-sun.cpp
+++ simple-sun.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
     int main()
     {
-         int x, y, soma;
-         scanf("%d", &x);
-         scanf("%d", &y);
-         soma = x + y;
+         int valorA, valorB, soma;
+         printf("Digite o primeiro numero: ");
+         scanf("%d", &valorA);
+         printf("Digite o primeiro numero: ")
+         scanf("%d", &valorB);
+         soma = valorA + valorB;
          printf("SOMA = %d\n", soma);
-    }                    
+    }  
