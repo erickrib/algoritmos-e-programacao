cb6bd5cf82f6a93c1ae68cd110269ed55c00b0fb
Erick Ribeiro <erickrocha965@gmail.com>
2022-02-17 10:11:33 -0300

    exercicios aula 4

---
+++ calc-media.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float notaA, notaB;
+    printf("Digite o valor da primeira nota: ");
+    scanf("%f", &notaA);
+    printf("Digite o valor da segunda nota: "
+    scanf("%f", &notaB);
+	med = (notaA + notaB)/2;
+	if (med > 9.5)
+		printf("Parabens! Aprovado");
+	else
+		print("Tente novamente! Reprovado")
+}
+
